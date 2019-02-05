170792bb0fef716d60e925546132ea97b20551a4
Lemke <lemkeb@fvtc.edu>
2019-02-05 12:22:47 -0600

    Stable home navigation update with home button added and spheres appearing and dissapearing when clicked on.

--- components.js
+++ components.js
@@ -7,6 +7,7 @@ AFRAME.registerComponent('location', {
         image360: {type: 'map'},
         video360: {type: 'selector'},
         signText: {type: 'string'},
+        planeWidth: {type: 'number', default: 2},
         yPlaneRotation: {type: 'number', default: 0},
         xShift: {type: 'number', default: .5},
         zShift: {type: 'number', default: 0}
@@ -17,11 +18,11 @@ AFRAME.registerComponent('location', {
     image360 is a map of the image that will be projected onto the sphere
     video360 is a selector that points to the video that is loaded in the assets. This is the video that should be loaded and played once this sphere is clicked on
     signText is the text that will appear below the sphere and infront of the plane. It should read the name of the location that will be visited
+    planeWidth is the width of the plane that is displayed behind the text. It is deafaulted to a value of 2 which should be suitable for most short names.
     yPlaneRotation is used to adjusted the rotation of the planes in 3D space
     xShift is used to move the planes relative to their spheres
     zShift is also used to move the planes relative to their spheres, but should not be used unless the desied result cannot be achieved using xShift alone.
     */
-
 
 
     //Initial creation of the mesh
@@ -46,13 +47,13 @@ AFRAME.registerComponent('location', {
         console.log(this.sphereMesh.position);
 
         //Create geometry for plane
-        this.planeGeometry = new THREE.PlaneGeometry( 2, .8, 32 );
+        this.planeGeometry = new THREE.PlaneGeometry( data.planeWidth, .8, 32 );
 
         //Create material for plane
-        this.planeMaterial = new THREE.MeshStandardMaterial({color:"#000000"});
+        this.planeMaterial = new THREE.MeshStandardMaterial({color:"#330000"});
 
         //Create mesh for plane
-        this.planeMesh = new THREE.Mesh(this.planeGeometry, this.plane);
+        this.planeMesh = new THREE.Mesh(this.planeGeometry, this.planeMaterial);
 
         //adjust the position
         this.planeMesh.position.set(data.positionX+data.xShift,data.positionY-1.4,data.positionZ+1+data.zShift);
@@ -88,54 +89,236 @@ AFRAME.registerComponent('location', {
         newText.setAttribute("position", positionValue)
         newText.setAttribute("rotation", rotationValue);
         sceneEl.appendChild(newText);
-        console.log(newText);
-
 
         var self = this;
         var sky = document.querySelector('a-sky');
 
         //Event listeners for controller interaction
-        el.addEventListener('triggerdown', function (evt) {
-            el.setAttribute('radius', 1.5);
-          });
-
-        el.addEventListener('mouseenter', function (evt) {
-            el.setAttribute('radius', 1.5);
-        });
-
         el.addEventListener('click', function (evt) {
             sky.setAttribute('src', self.data.video360.src);
             sky.setAttribute('color', "#FFFFFF");
             console.log(self.data.video360);
+
+
+            ////////Once any visible sphere has been clicked for a location, hide the spheres, planes and text
+
+            //Check if the sphere is visible
+            if(el.getAttribute('visible') == true){
+                //Get an array of all of the sphere and loop through them setting their visibility to false
+                var spheres = document.querySelectorAll(".locationSphere");
+                for(var i=0; i<spheres.length; i++){
+                    spheres[i].setAttribute("visible", false);
+                }
+
+                var navPlanes = document.querySelectorAll(".navigationPlane");
+                for(var i=0; i<navPlanes.length; i++){
+                    navPlanes[i].setAttribute("visible", false);
+                }
+
+                var navText = document.querySelectorAll(".navigationText");
+                for(var i=0; i<navText.length; i++){
+                    navText[i].setAttribute("visible", false);
+                }
+
+                //Hide the ground and home background
+                var ground = document.querySelector("#ground");
+                ground.setAttribute("visible", false);
+                var homeBackground = document.querySelector("#homeBackground");
+                homeBackground.setAttribute("visible", false);
+
+                //Make the home button and label visisble
+
+                var homebutton = document.querySelector(".homebutton");
+                homebutton.setAttribute("visible", true);
+
+                var homebuttonPlane = document.querySelector(".homeNavigationPlane");
+                homebuttonPlane.setAttribute("visible", true);
+
+                var homebuttonText = document.querySelector(".homeNavigationText");
+                homebuttonText.setAttribute("visible", true);
+            }
+
         });
     },
 
+    update: function (){
 
-    //Update the mesh in response to property updates.
-    update: function (oldData){
-        var data = this.data;
-        var el = this.el;
-
-        //If oldData is empty, then we're creating and we don't need to update
-        if (Object.keys(oldData).length === 0){return;}
-
-        //Geometry related property changes:
-        if(data.width !== oldData.width || data.height !== oldData.height || data.depth !== oldData.depth)
-        {
-            //Update geometry
-            el.getObject3D('mesh').geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
-        }
-
-        //Material related property changes:
-        if(data.color !== oldData.color)
-        {
-            el.getObject3D('mesh').material.color = data.color;
-        }
     },
 
     remove: function(){
-        this.el.removeObject3D('mesh');
     }
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+AFRAME.registerComponent('homebutton', {
+    //Define the schema for the box    
+    schema: {
+        positionX: {type: 'number'},
+        positionY: {type: 'number'},
+        positionZ: {type: 'number'},
+        image: {type: 'map'},
+        signText: {type: 'string'},
+        yPlaneRotation: {type: 'number', default: 0},
+        xShift: {type: 'number', default: .5},
+        zShift: {type: 'number', default: 0}
+    },
+
+    /*
+    positionX, Y, and Z are used to position the Sphere in space on the plane
+    image is a map of the image that will be projected onto the sphere
+    signText is the text that will appear below the sphere and infront of the plane. It should read the name of the location that will be visited
+    yPlaneRotation is used to adjusted the rotation of the planes in 3D space
+    xShift is used to move the planes relative to their spheres
+    zShift is also used to move the planes relative to their spheres, but should not be used unless the desied result cannot be achieved using xShift alone.
+    */
+
+
+    //Initial creation of the mesh
+    init: function(){
+        var data = this.data;
+        var el = this.el;
+        var sceneEl = document.querySelector('a-scene');
+
+        //Create geometry for sphere
+        this.sphereGeometry = new THREE.SphereBufferGeometry( 1, 32, 32 );
+
+        //Create material for sphere
+        texture = new THREE.TextureLoader().load(data.image.src);
+        this.sphereMaterial = new THREE.MeshBasicMaterial( { map: texture } );
+
+        //Create mesh for sphere
+        this.sphereMesh = new THREE.Mesh(this.sphereGeometry, this.sphereMaterial);
+        this.sphereMesh.position.set(data.positionX,data.positionY,data.positionZ);
+
+        //rotate the sphere's mesh so the home symbol faces the camera
+        this.sphereMesh.rotation.y = -Math.PI*3/4;
+
+        console.log(data.signText);
+        console.log(this.sphereMesh.position);
+
+        //Create geometry for plane
+        this.planeGeometry = new THREE.PlaneGeometry( 2, .8, 32 );
+
+        //Create material for plane
+        this.planeMaterial = new THREE.MeshStandardMaterial({color:"#330000"});
+
+        //Create mesh for plane
+        this.planeMesh = new THREE.Mesh(this.planeGeometry, this.planeMaterial);
+
+        //adjust the position
+        this.planeMesh.position.set(data.positionX+data.xShift,data.positionY-1.4,data.positionZ+1+data.zShift);
+        this.planeMesh.rotateY(THREE.Math.degToRad(data.yPlaneRotation));
+        
+        var newPlane = document.createElement('a-entity');
+        
+        //set mesh on entity
+        el.setObject3D('mesh', this.sphereMesh);
+        newPlane.setObject3D('mesh', this.planeMesh)
+        
+
+        //Give the entity a class we can refer to it by later
+        newPlane.setAttribute("class", "homeNavigationPlane");
+        console.log(newPlane);
+        sceneEl.appendChild(newPlane);
+    
+
+        //Create the text and set its attributes
+        var rotationValue = "0 " + data.yPlaneRotation + " 0";
+        console.log(rotationValue);
+
+        var positionValue = String(data.positionX + data.xShift) + " " +  String(data.positionY -1.4) + " " + String(data.positionZ+1.1+data.zShift);
+        console.log(positionValue)
+        var newText = document.createElement('a-entity');
+
+        newText.setAttribute("text", {"value": data.signText,
+                                    "width": 10,
+                                    "height": 10,
+                                    "align": "center"
+                                    });
+        newText.setAttribute("class", "homeNavigationText");
+        newText.setAttribute("position", positionValue);
+        newText.setAttribute("rotation", rotationValue);
+        sceneEl.appendChild(newText);
+
+        var sky = document.querySelector('a-sky');
+
+        //Make this home button invisible on startup
+        resetHomeScreen();
+ 
+
+        //Event listeners for controller interaction
+        el.addEventListener('click', function (evt) {
+
+            sky.setAttribute('color', "#FFFFFF");
+
+            //Once the home sphere has been clicked (if visisble), show the spheres, planes and text
+
+
+            //check if the home button is visible
+            if(el.getAttribute('visible') == true){
+                //Get an array of all of the spheres and loop through them setting their visibility to true
+                var spheres = document.querySelectorAll(".locationSphere");
+                for(var i=0; i<spheres.length; i++){
+                    console.log(spheres[i]);
+                    spheres[i].setAttribute("visible", true);
+                }
+
+                var navPlanes = document.querySelectorAll(".navigationPlane");
+                for(var i=0; i<navPlanes.length; i++){
+                    navPlanes[i].setAttribute("visible", true);
+                }
+
+                var navText = document.querySelectorAll(".navigationText");
+                for(var i=0; i<navText.length; i++){
+                    navText[i].setAttribute("visible", true);
+                }
+
+                //hide the home sphere and plane and text
+                resetHomeScreen();
+            }
+
+        });
+    },
+
+    update: function (){
+
+    },
+
+    remove: function(){
+    }
+
+});
+
+
+
+function resetHomeScreen() {
+    var homebutton = document.querySelector(".homebutton");
+    homebutton.setAttribute("visible", false);
+    var homebuttonPlane = document.querySelector(".homeNavigationPlane");
+    homebuttonPlane.setAttribute("visible", false);
+    var homebuttonText = document.querySelector(".homeNavigationText");
+    homebuttonText.setAttribute("visible", false);
+
+    var ground = document.querySelector("#ground");
+    ground.setAttribute("visible", true);
+
+    var homeBackground = document.querySelector("#homeBackground");
+    homeBackground.setAttribute("visible", true);
+}
 
