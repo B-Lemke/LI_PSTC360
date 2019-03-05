ad43c66093e61944899685c8bf11ae94a59d937a
Lemke <lemkeb@fvtc.edu>
2019-03-05 08:32:00 -0600

    Device detection with controllers, moving to the other computer.

--- detectDevice.js
+++ detectDevice.js
@@ -2,19 +2,27 @@ AFRAME.registerComponent('device_detector', {
   schema: {},
   init: function () {
 
-    //If the device is a mobile phone
+    var el = this.el;
+    this.controllerCount = 0;
+
+
+    //Check if the device is mobile or not and set a bool
     if(AFRAME.utils.device.isMobile()){
+        //Reticle with fuse time out and click
 
-    }
+        
 
-    //If the device is not a mobile phone
-    if(!AFRAME.utils.device.isMobile()){
-        console.log("Not mobile");
+    } else{
+        //Browser. Click controls, no reticle
+
+
+
+
     }
 
     //If the device is GearVR
     if(AFRAME.utils.device.isGearVR()){
-
+        
     }
 
     if(AFRAME.utils.device.isOculusGo ()){
@@ -22,7 +30,21 @@ AFRAME.registerComponent('device_detector', {
     }
 
     //Check headset connected 
-    if(AFRAME.utils.device.checkHeadsetConnected()){
+    if(AFRAME.utils.device.checkHeadsetConnected()){        
+        
+        el.addEventListener('controllerConnected', function(){
+            this.controllerCount += 1;
+            console.log('controller connected and emitted the event on the scene');
+            console.log(this.controllerCount);
+        });
+        el.addEventListener('controllerDisconnected', function(){
+            this.controllerCount -= 1;
+            console.log('controller disconnected and emitted the event on the scene');
+            console.log(this.controllerCount);
+        });
+
+
+
 
     }
 
@@ -50,3 +72,23 @@ AFRAME.registerComponent('device_detector', {
   }
 
 });
+
+
+
+//Controller connected component
+AFRAME.registerComponent('controller_connected', {
+    init: function () {
+      var el = this.el;
+      el.addEventListener('controllerconnected', function (evt) {
+        var scene = document.querySelector("a-scene");
+        scene.emit('controllerConnected');
+      });
+
+      el.addEventListener('controllerdisconnected', function (evt) {
+        var scene = document.querySelector("a-scene");
+        scene.emit('controllerDisconnected');
+      });
+
+    } 
+});
+  
